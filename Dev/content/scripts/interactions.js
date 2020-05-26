5c4fadcd3b10c077baeb6faa2122426dc2059edd
csabourin <christian.sabourin@canada.ca>
2020-05-26 14:48:58 -0400

    Initial commit

---
+++ Dev/content/scripts/interactions.js
@@ -0,0 +1,109 @@
+//DO NOT MODIFY ↓
+define([
+    'jquery'
+], function($) {
+//DO NOT MODIFY ↑
+
+	
+(function($){
+ 
+    $.fn.shuffle = function() {
+ 
+        var allElems = this.get(),
+            getRandom = function(max) {
+                return Math.floor(Math.random() * max);
+            },
+            shuffled = $.map(allElems, function(){
+                var random = getRandom(allElems.length),
+                    randEl = $(allElems[random]).clone(true)[0];
+                allElems.splice(random, 1);
+                return randEl;
+           });
+ 
+        this.each(function(i){
+            $(this).replaceWith($(shuffled[i]));
+        });
+ 
+        return $(shuffled);
+ 
+    };
+ 
+})(jQuery);	
+
+function exceptionOne(){
+	$("#q1-a").after("<textarea  class=\"col-xs-12\"  rows=\"6\">Il a dit</textarea>");
+	$("#q1-a").remove();
+	
+}
+
+
+function exceptionTwo(){
+	$("p.col-xs-5").removeClass("col-xs-5")
+	$(".qs-answers").find("label").css("float", "left").css("padding-bottom", "20px")
+}
+function exceptionThree(){
+	$("p.col-xs-5").addClass("col-xs-4")
+	$("p.col-xs-5").removeClass("col-xs-5");
+	}
+
+	function initialize() {
+		setEvents();
+	}
+
+	function setEvents() {
+		$(masterStructure)
+			.on("Framework:systemReady", function() {
+				systemReady();
+			})
+			.on("Framework:pageLoaded", function() {
+				pageLoaded();
+			});
+	}
+
+	/* is called only once, when the Course has loaded*/
+	function systemReady() {
+		//console.log("Interactions:systemReady");
+
+
+	}
+
+	/* is called on every page load, great for adding custom code to all pages*/
+	function pageLoaded() {
+
+		
+		//console.log("Interactions:pageLoaded");
+
+		var medias = [];
+		medias = Array.prototype.concat.apply(medias, document.getElementsByTagName('audio'));
+		medias = Array.prototype.concat.apply(medias, document.getElementsByTagName('video'));
+		
+		$(medias).each(function(index){
+			var catched = false;
+			$(this).on('playing',function(e){							
+				if(!catched){		        
+					catched = true; 					
+					window.ga('send',{
+						hitType:'event',
+						eventCategory:this.nodeName,
+						eventAction:'play',
+						eventLabel:this.getAttribute('title')
+					})
+		     	}
+			});
+		});		
+	}
+
+	initialize();
+
+});
+
+//Javascript document
+/* is called on every page load, great for adding custom code to all pages*/
+function pageLoaded(){
+
+
+	}
+
+
+
+
---
+++ Dev/settings/settings-general.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+define({
+  "courseTitle_en": "Power Series",
+  "courseTitle_fr": "Série Pouvoir",
+  "courseSubtitle_en": "",
+  "courseSubtitle_fr": "",
+  "seriesTitle_en": "Power Series",
+  "seriesTitle_fr": "Série Pouvoir",
+  "addToolboxPage": [],
+  "editMode": true,
+  "showLangSwitch":false,
+  "showHelp":false
+});
